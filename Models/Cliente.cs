98155c740394419c1e6d7e51f08a6908c0bb0934
unknown <you@example.com>
2022-07-06 22:51:29 -0300

    enviando arquivos base do projeto

---
+++ Models/Cliente.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankAccountModule.Models
+{
+    public class Cliente
+    {
+    string nome;
+    int cpf;
+    int dataNascimento;
+    string profissao;
+        
+    }
+}
---
+++ Models/ContaCorrente.cs
@@ -0,0 +1,12 @@
+namespace BankAccountModule.Models
+{
+// Dados conta corrente
+class ContaCorrente
+{
+    int numero;
+    int agencia;
+    float saldo;
+    string titular;
+
+}
+}
